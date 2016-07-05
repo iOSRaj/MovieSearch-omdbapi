6ee12a246f0df6a6137e2e15d0d54e319326f3c1
Raj <rajkumar.kothandaraman@klm.com>
2016-07-05 21:14:49 +0200

    Initial Commit

---
+++ MovieSearch-omdbapi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieSearch-omdbapi
+//
+//  Created by tcs on 7/5/16.
+//  Copyright © 2016 Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
